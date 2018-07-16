0676acc37e029b0dbfec19d4e20211b9046d790c
aalfodel <aalfodel@OPENGATE>
2018-07-16 17:54:00 +0200

    First commit
    Just a basic ZK page with bindings

---
+++ CMSPROT_ZK_FragmentsGen_PageAsm/src/main/java/CMSPROT/CMSPROT_ZK_FragmentsGen_PageAsm/CMSPROT_3StepsViewModel.java
@@ -0,0 +1,28 @@
+package CMSPROT.CMSPROT_ZK_FragmentsGen_PageAsm;
+
+public class CMSPROT_3StepsViewModel {
+	String id;
+	String text;
+	String color;
+	
+	//GETTERS SETTERS
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+}
