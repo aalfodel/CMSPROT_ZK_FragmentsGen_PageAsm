d98a6ab61586af9d7d45a05d980f3c0e77677bfd
aalfodel <aalfodel@OPENGATE>
2018-07-17 09:47:42 +0200

    Added button

--- CMSPROT_ZK_FragmentsGen_PageAsm/src/main/java/CMSPROT/CMSPROT_ZK_FragmentsGen_PageAsm/CMSPROT_3StepsViewModel.java
+++ CMSPROT_ZK_FragmentsGen_PageAsm/src/main/java/CMSPROT/CMSPROT_ZK_FragmentsGen_PageAsm/CMSPROT_3StepsViewModel.java
@@ -1,5 +1,7 @@
 package CMSPROT.CMSPROT_ZK_FragmentsGen_PageAsm;
 
+import org.zkoss.bind.annotation.Command;
+
 public class CMSPROT_3StepsViewModel {
 	private String id;
 	private String text;
@@ -23,6 +25,11 @@ public class CMSPROT_3StepsViewModel {
 	}
 	public void setColor(String color) {
 		this.color = color;
+	}
+	
+	@Command("genPage")
+	public void generatePage() {
+		System.out.println("OK");
 	}
 	
 }
